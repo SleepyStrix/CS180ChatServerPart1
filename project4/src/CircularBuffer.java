0de044e4723231e0a9f977b1916830b68a236e7e
Daniel Hewlett <DanCHewlett@gmail.com>
2015-11-18 18:28:40 -0500

    Initial Commit

---
+++ project4/src/CircularBuffer.java
@@ -0,0 +1,84 @@
+import java.text.DecimalFormat;
+
+/**
+ * Created by Dan on 11/15/2015.
+ */
+public class CircularBuffer {
+
+    private String[] buffer;
+    private int pointer = 0;
+    private int messageCount = 0;
+    private int numAvailable = 0;
+
+
+    public CircularBuffer(int size) {
+        this.buffer = new String[size];
+    }
+
+    public void put(String message) {
+        DecimalFormat df = new DecimalFormat("0000");
+        String fourDigit = df.format(messageCount);
+        String result = fourDigit + ") " + message;
+        buffer[pointer] = result;
+        messageCountUp();
+        pointerUp();
+        if (numAvailable < buffer.length) {
+            numAvailable++;
+        }
+
+    }
+
+    public String[] getNewest(int numMessages) {
+        if (numMessages < 0) {
+            return null;
+        }
+        if (numMessages == 0) {
+            return new String[0];
+        }
+        int minNum = Math.min(numAvailable, numMessages);
+        if (minNum == 0) {
+            return new String[0];
+        }
+        String[] result = new String[minNum];
+        int count = 0;
+        for (int i = lastPointer(pointer); i <= minNum;) {
+            result[count] = buffer[i];
+            i = lastPointer(i);
+            count++;
+        }
+        return result;
+    }
+
+    private void pointerUp() {
+        pointer = nextPointer(pointer);
+    }
+
+    private void messageCountUp() {
+        messageCount = nextMessageCount();
+    }
+
+    private int nextPointer(int from) {
+        int next = from + 1;
+        if (next >= buffer.length) {
+            next = 0;
+        }
+        return next;
+    }
+
+    private int nextMessageCount() {
+        int next = messageCount + 1;
+        if (next > 9999) {
+            next = 0;
+        }
+        return next;
+    }
+
+    private int lastPointer(int from) {
+        int next = from - 1;
+        if (next == 0) {
+            next = buffer.length - 1;
+        }
+        return next;
+    }
+
+}
